6b6afc673ca1ca7cabb9b980f68b545135c0ffa3
Ruksan karki <rukshankarki80@gmail.com>
2025-08-21 08:01:22 +0545

    learning about data types and Special case about object when we use const in this case

---
+++ datatype.js
@@ -0,0 +1,27 @@
+// datatypes
+// 1.string
+
+let username = "Ruksan karki";
+console.log(username);
+// 2.number
+let userage = 22;
+console.log(userage);
+// 3. boolean
+let ispass = true;
+console.log(ispass);
+
+//.4 undefined
+
+let useraddress = undefined;
+console.log(useraddress);
+
+// 5. object
+const user = {
+    name: "Ruksan",
+    age: 22,
+    isactive: true
+};
+user.user = "rubisha karki ";
+user.name = "Ruksan karki";
+console.log(user);
+
--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 console.log("Ruksan");
 console.log("karki");
+
+let x = 20;
+{
+    let y = 30;
+}
+console.log("using variable   ", x);
+
+console.log("using variable   ", y);
+
+let z = 40;
+console.log("using variable" , z);
+
+
