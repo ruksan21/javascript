e8e6b6074b0b5037e1914171cd8936d8f3537c16
Ruksan karki <rukshankarki80@gmail.com>
2025-08-22 07:30:33 +0545

    null symbol and bigint done.

--- datatype.js
+++ datatype.js
@@ -25,3 +25,19 @@ user.user = "rubisha karki ";
 user.name = "Ruksan karki";
 console.log(user);
 
+//.6 array
+const users = ["Ruksan", "Rubisha", "John"];
+console.log(users);
+
+//.7 Null
+let useremail = null;
+console.log(useremail);
+
+//.8 Symbol
+const sym1 = Symbol("Ruksan");
+const sym2 = Symbol("Ruksan");
+console.log(sym1 == sym2);
+
+//.9 BigInt
+const bigInt = 1234567890123456789012345678901234567890n;
+console.log(bigInt);
