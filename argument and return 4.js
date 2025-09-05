c88e6df85dc5bfc8afe14e4d1f80b1ee6db58cef
Ruksan karki <rukshankarki80@gmail.com>
2025-09-05 07:16:26 +0545

    using array

---
+++ argument and return 4.js
@@ -0,0 +1,1 @@
+// write a function that takes a number as an argument and returns is factor test the diffrence of a number.
---
+++ labwork/array.js
@@ -0,0 +1,19 @@
+const student = [1];
+student[0] = "Ruksan";
+student[1] = "Rubisha";
+student[2] = "John";
+student[3] = "Doe";
+console.log(`student no ${student.length} name is ${student[student.length -1]}`);
+
+// let using
+
+let a = 10;
+a = 20 ;
+console.log(a);
+
+//
+let arr = [1,2,3,4,5];
+console.log(arr);
+console.log(arr.toString());
+console.log(arr.length);
+
