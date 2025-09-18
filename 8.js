bc921723318ca1d810e7ce78c8872525723162f7
Ruksan karki <rukshankarki80@gmail.com>
2025-09-18 08:07:23 +0545

    Add examples demonstrating the use of shift and unshift methods in arrays

---
+++ 8.js
@@ -0,0 +1,10 @@
+// shift can help to remove the first element of an array
+const arr = [1, 2, 3, 4, 5];
+arr.shift();
+console.log(arr);
+
+
+// unshift can help to add an element at the beginning of an array
+const arr1 = [1, 2, 3, 4, 5];
+arr1.unshift(0);
+console.log(arr1);
